3912b7cec9212ea46fe58269c069c3a8a2223222
Raunaq Jahan <raunaq.jahan60@gmail.com>
2026-04-24 19:18:44 +0600

    update

--- server.js
+++ server.js
@@ -271,4 +271,7 @@ app.post('/generate-questions', upload.single('question_file'), (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
